68dad0e0897c686bdac394fd784f17f9c8629dbe
ray1028 <rayjiang@Rays-MacBook-Pro.local>
2019-09-18 16:10:57 -0400

    completing ceature/dashboard-counts

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,6 +1,8 @@
 class Admin::DashboardController < ApplicationController
-  http_basic_authenticate_with name: ENV['USERNAME'], password: ENV['PASSWORD'],
-  only: [:show]
+  # http_basic_authenticate_with name: ENV['USERNAME'], password: ENV['PASSWORD'],
+  # only: [:show]
   def show
+    @total_products = Product.all.count
+    @total_categories = Category.all.count
   end
 end
